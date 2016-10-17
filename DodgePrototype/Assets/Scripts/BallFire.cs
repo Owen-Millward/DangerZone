5fa3dc2197ece30cb12bb00189ae9aa0cdfdb164
owenm144 <owen.millward24@gmail.com>
2016-10-17 13:42:32 +0100

    Script changes
    Adds a prefab for a ball to be thrown at the target when instantiated
    (not finished).
    
    Edits the other scripts for readability and makes some changes to the
    structure.

---
+++ DodgePrototype/Assets/Scripts/BallFire.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallFire : MonoBehaviour {
+
+	// Movement variables
+	public float speed;
+
+	bool moving = true;
+
+	private Vector3 direction;
+
+	private Rigidbody body;
+	private GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+		body = GetComponent<Rigidbody>();
+		target = GameObject.FindWithTag("Player");
+
+		// Init movement variables
+		moving = true;
+
+		//calculate direction ( target position - current position)
+		direction = target.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Move positively in the X axis
+		if (moving) {
+			body.velocity = direction * speed;
+		}
+	}
+}
---
+++ DodgePrototype/Assets/Scripts/BallThrow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallThrow : MonoBehaviour {
+
+	private Rigidbody body;
+	private Transform target;
+
+	public float velocity;
+	public float distance;
+	public Vector3 direction;
+	public Vector3 directionThrow;
+	public float scale;
+
+	// Use this for initialization
+	void Start () {
+
+		body = GetComponent<Rigidbody>();
+		target = (GameObject.FindWithTag("Player").transform);
+		
+		ThrowBall();
+	}
+
+	void ThrowBall (){
+
+		// Calculate direction and distance of target
+		direction = (target.position - transform.position).normalized;
+		distance = Vector3.Distance(transform.position, target.transform.position);
+
+		// Decide a firing angle based on distance (for now use 45)
+		float theta = 45.0f;
+
+		// Calculate velocity required to hit target
+		velocity = Mathf.Sqrt(distance / (Mathf.Sin(2 * theta * Mathf.Deg2Rad) / 9.8f));
+
+		// Horizontal and Vertical components of velocity
+		float Vy = velocity * Mathf.Sin(theta * Mathf.Deg2Rad);
+		float Vx = velocity * Mathf.Cos(theta * Mathf.Deg2Rad);
+		
+		// X and Z components
+		//float Vx = velocity_h * Mathf.Cos(theta * Mathf.Deg2Rad);
+		//float Vz = velocity_h * Mathf.Sin(theta * Mathf.Deg2Rad);
+
+		// Direction of throw = Velocity required to hit target
+		directionThrow = new Vector3(Vx, Vy, 0);
+		body.AddForce(directionThrow * velocity * scale);
+	}
+}
